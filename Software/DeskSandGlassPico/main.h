cce31098b2a92eef7f60fccaaaed4a6014004259
RSTurgay <turgay@realsolutions.com.tr>
2023-08-05 14:14:01 +0300

    First Init

---
+++ Software/DeskSandGlassPico/main.h
@@ -0,0 +1,20 @@
+//
+// Created by turga on 17.03.2023.
+//
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+#include <pico/stdio.h>
+#include <pico/time.h>
+#include <pico/types.h>
+#include "pico/binary_info.h"
+#include <hardware/clocks.h>
+#include <hardware/gpio.h>
+#include <hardware/uart.h>
+#include <hardware/adc.h>
+#include <hardware/i2c.h>
+
+#ifndef INC_MAIN_H
+#define INC_MAIN_H
+
+#endif //INC_MAIN_H
